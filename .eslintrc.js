208aeed82ce04460cc06f431ec83e3d5ef8c7fa3
LeDuyTan <tanb1910699@student.ctu.edu.vn>
2022-10-23 16:58:33 +0700

    Implement handlers

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,11 @@
 module.exports = {
-  env: {
-    node: true,
-    commonjs: true,
-    es2022: true,
-  },
-  extends: "eslint:recommended",
-  rules: {
-    "no-unused-vars": "off",
-  },
-};
+    env: {
+         node: true,
+         commonjs: true,
+         es2822: true
+    },
+    extends: 'eslint:recommended',
+    rules: {
+         "no-unused-vars": "off"
+    }
+}
--- src/app.js
+++ src/app.js
@@ -1,35 +1,33 @@
-const express = require("express");
-const cors = require("cors");
-
-const ApiError = require("./api-error");
-
+const express = require('express');
+const cors = require('cors');
 const app = express();
-
-const contactController = require("./controllers/contact.controller");
+const contactController = require('./controllers/contact.controller');
+const ApiError = require('./api-error');
+// const { application } = require('express');
 
 app.use(cors());
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ message: `Welcome to contact book application.` });
+app.get('/', (req, res) => {
+    res.json ({ message: 'Welcome to contact book application.'});
 });
 
-app.route("/api/contacts")
-  .get(contactController.findAll)
-  .post(contactController.create)
-  .delete(contactController.deleteAll);
+app.route('/api/contacts')
+    .get(contactController.findAll)
+    .post(contactController.create)
+    .delete(contactController.deleteAll);
 
-app.route("/api/contacts/favorite").get(contactController.findAllFavorite);
+app.route('/api/contacts/favorite').get(contactController.findAllFavorite);
 
-app.route('/api/contacts/:id(\\d+)')
-.get(contactController.findOne)
-.put(contactController.update)
-.delete(contactController.delete);
+app.route('/api/contatcs/:id')
+    .get(contactController.findOne)
+    .put(contactController.update)
+    .delete(contactController.delete);
 
-app.route("/api/contacts/:id")
-  .get(contactController.findOne)
-  .put(contactController.update)
-  .delete(contactController.delete);
+app.route('/api/contacts/:id')
+    .get(contactController.findOne)
+    .put(contactController.update)
+    .delete(contactController.delete);
 
 app.use((req, res, next) => {
     return next(new ApiError(404, 'Resource not found'));
